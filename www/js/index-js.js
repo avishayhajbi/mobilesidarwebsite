9560f5870aeae06f5ada866adcaac8081a0354f5
avishayhajbi <avishayhajbi@gmail.com>
2014-01-26 11:51:22 +0200

    4 columns

--- www/js/index-js.js
+++ www/js/index-js.js
@@ -1,29 +1,5 @@
 /* rotate screen */
-$(document).ready(function() {
-    window.addEventListener("orientationchange", function() {
-       alert(window.orientation);
-    });
-});
 
-var OrientationLock = function() {}
-
-OrientationLock.prototype.lock = function(orientation, success, fail) {
-        return cordova.exec(success, fail, "OrientationLock", "lock", [orientation])
-}
-
-OrientationLock.prototype.unlock = function(success, fail) {
-        return cordova.exec(success, fail, "OrientationLock", "unlock", [])
-}
-
-if ( ! window.plugins ) {
-  window.plugins = {}
-}
-if ( ! window.plugins.orientationLock ) {
-  window.plugins.orientationLock = new OrientationLock()
-}
-
-module.exports = OrientationLockrn [windowWidth, windowHeight];
-}
 /* end rotate screen */
 
 /*menu*/
