b2ced49ea2fce9e11abab4dd55f15ac9347e89b9
avishayhajbi <avishayhajbi@gmail.com>
2014-01-29 14:52:16 +0200

    with navigation bar

--- www/js/index-js.js
+++ www/js/index-js.js
@@ -3,6 +3,11 @@
 /* end rotate screen */
 
 /*menu*/
+$(document).ready(function() {
+    $( "#nav_img" ).click(function() {
+    $( "#nav_bar" ).slideToggle( "slow" );
+  });
+});
 
 /*end menu*/
 
