019603623bd4c3039fa78a9b985bd4819e27b424
avishayhajbi <avishayhajbi@gmail.com>
2014-02-02 11:15:49 +0200

    great nav

---
+++ www/www/js/index-js.js
@@ -0,0 +1,13 @@
+/* rotate screen */
+
+/* end rotate screen */
+
+/*menu*/
+$(document).ready(function() {
+    $( "#nav_img" ).click(function() {
+    $( "#nav_bar" ).slideToggle( "slow" );
+  });
+});
+
+/*end menu*/
+
