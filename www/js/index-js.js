69fed404269aee6168328faa1670a0928e0a85bd
avishayhajbi <avishayhajbi@gmail.com>
2014-02-02 15:23:54 +0200

    fix menu and sub menu

--- www/js/index-js.js
+++ www/js/index-js.js
@@ -3,11 +3,33 @@
 /* end rotate screen */
 
 /*menu*/
-$(document).ready(function() {
-    $( "#nav_img" ).click(function() {
-    $( "#nav_bar" ).slideToggle( "slow" );
-  });
+ $(document).ready(function(){
+
+	$('#cssmenu > ul > li:has(ul)').addClass("has-sub");
+	$('#cssmenu > ul > li > a').click(function() {
+
+	var checkElement = $(this).next();
+
+	$('#cssmenu li').removeClass('active');
+	$(this).closest('li').addClass('active');
+
+	if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+		$(this).closest('li').removeClass('active');
+		checkElement.slideUp('normal');
+	}
+
+	if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+		$('#cssmenu ul ul:visible').slideUp('normal');
+		checkElement.slideDown('normal');
+	}
+
+	if (checkElement.is('ul')) {
+		return false;
+	} else {
+		return true;
+	}
 });
 
+});
 /*end menu*/
 
