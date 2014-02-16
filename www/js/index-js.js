ef0466a6c35c1fb6c0a1f2c81163365bc94c9403
avishayhajbi <avishayhajbi@gmail.com>
2014-02-16 18:40:33 +0200

    another version for preventing page scrolling

--- www/js/index-js.js
+++ www/js/index-js.js
@@ -1,55 +1,80 @@
 /* scroll screen */
- $(document).ready(function(){
-    touchMove = function(event) {
-        event.preventDefault();
-    }
-    });
+
+$(document).ready(function() {
+	touchMove = function(event) {
+		event.preventDefault();
+		scrolling_page();
+	};
+});
+/*scroll body*/
+
+$(document).ready(function() {
+	scrolling_page();
+});
+var checkscrollingpage = 1;
+function scrolling_page() {
+	if (checkscrollingpage == 0) {
+		$('html, body').css({
+			'overflow' : 'hidden',
+			'height' : '100%'
+		});
+		checkscrollingpage = 1;
+	} else {
+		$('html, body').css({
+			'overflow' : 'auto',
+			'height' : 'auto'
+		});
+		checkscrollingpage = 0;
+	}
+}
+
+/*end scroll body*/
 /* end scroll screen */
 
 /*menu*/
- $(document).ready(function(){
+$(document).ready(function() {
 
 	$('#cssmenu > ul > li:has(ul)').addClass("has-sub");
 	$('#cssmenu > ul > li > a').click(function() {
 
-	var checkElement = $(this).next();
+		var checkElement = $(this).next();
 
-	$('#cssmenu li').removeClass('active');
-	$(this).closest('li').addClass('active');
+		$('#cssmenu li').removeClass('active');
+		$(this).closest('li').addClass('active');
 
-	if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-		$(this).closest('li').removeClass('active');
-		checkElement.slideUp('normal');
-	}
+		if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+			$(this).closest('li').removeClass('active');
+			checkElement.slideUp('normal');
+		}
 
-	if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-		$('#cssmenu ul ul:visible').slideUp('normal');
-		checkElement.slideDown('normal');
-	}
+		if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+			$('#cssmenu ul ul:visible').slideUp('normal');
+			checkElement.slideDown('normal');
+		}
 
-	if (checkElement.is('ul')) {
-		return false;
-	} else {
-		return true;
-	}
-});
+		if (checkElement.is('ul')) {
+			return false;
+		} else {
+			return true;
+		}
+	});
 
 });
 /*end menu*/
 
 /* page transport*/
-$(document).on('pagebeforeshow', '#page', function(){ 
-    $(document).on('click', '.home_page_img', function(){    
-    	/*$.mobile.changePage( "searchresults.php", {
-		  type: "post",
-		  data: $("form#search").serialize()
+$(document).on('pagebeforeshow', '#page', function() {
+	$(document).on('click', '.home_page_img', function() {
+		/*$.mobile.changePage( "searchresults.php", {
+		type: "post",
+		data: $("form#search").serialize()
 		});*/
-        //$.mobile.navigate( "workinfo.html", { transition : "pop", info: "" });
-        //$.mobile.changePage( "workinfo.html", { transition: "slideup"});
-    });   
+		//$.mobile.navigate( "workinfo.html", { transition : "pop", info: "" });
+		//$.mobile.changePage( "workinfo.html", { transition: "slideup"});
+	});
 });
 /* end page transport */
 
-$('.information').on("expand", function(e){
-    $(e.target + '.ui-btn').addClass('ui-btn-active').collapsible("refresh");
+$('.information').on("expand", function(e) {
+	$(e.target + '.ui-btn').addClass('ui-btn-active').collapsible("refresh");
 });
